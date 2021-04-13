5dac2fdb2f3e403048f1a3b8ff2d8d2345ffbcb6
blargeth <albertkchua@gmail.com>
2021-04-13 18:25:07 -0400

    adds blue to the start and slows the duration

--- src/components/redcircle.js
+++ src/components/redcircle.js
@@ -2,12 +2,10 @@ import {useSpring, animated} from 'react-spring';
 
 function RedCircle() {
   const props = useSpring({
-    opacity: 1,
-    from: {opacity: 0, marginLeft: -100},
-    to: {opacity: 1, marginLeft: 0}
+    config: {duration: 3000},
+    from: {opacity: 0, marginLeft: -100, backgroundColor: "blue", number: 0},
+    to: {opacity: 1, marginLeft: 0, backgroundColor: "red", number: 3}
   })
-
-
 
   return <animated.div style={props} className="redCircle" ></animated.div>
 }
