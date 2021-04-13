f3719c205b7cf4484f60489931401bbe6b7906e7
blargeth <albertkchua@gmail.com>
2021-04-13 17:07:28 -0400

    created a fading component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Component1 from './components/component1';
 
 function App() {
   return (
     <div className="App">
       The beginning of the Playground!
+
+    <Component1/>
+
+
+
     </div>
   );
 }
